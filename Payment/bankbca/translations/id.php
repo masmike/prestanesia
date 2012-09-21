96eceb65367d7ef4ed2d59fe63384d574b31104c
afasyah <afasyah@gmail.com>
2012-09-21 16:39:06 +0000

    Initial version

---
+++ Payment/bankbca/translations/id.php
@@ -0,0 +1,24 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{bankbca}prestashop>validation_e2b7dec8fa4b498156dfee6e4c84b156'] = 'Questo metodo di pagamento non è disponibile.';
+$_MODULE['<{bankbca}prestashop>payment_execution_f1d3b424cd68795ecaa552883759aceb'] = 'Sintesi dell\'ordine';
+$_MODULE['<{bankbca}prestashop>payment_execution_879f6b8877752685a966564d072f498f'] = 'Il carrello è vuoto.';
+$_MODULE['<{bankbca}prestashop>payment_execution_c884ed19483d45970c5bf23a681e2dd2'] = 'Ecco un breve riepilogo del tuo ordine:';
+$_MODULE['<{bankbca}prestashop>payment_execution_e2867a925cba382f1436d1834bb52a1c'] = 'L\'importo totale del tuo ordine è';
+$_MODULE['<{bankbca}prestashop>payment_execution_1f87346a16cf80c372065de3c54c86d9'] = '(tasse incl.)';
+$_MODULE['<{bankbca}prestashop>payment_execution_a7a08622ee5c8019b57354b99b7693b2'] = 'Scegli una delle seguenti:';
+$_MODULE['<{bankbca}prestashop>payment_execution_0881a11f7af33bc1b43e437391129d66'] = 'Conferma l\'ordine cliccando su \'confermo il mio ordine\'';
+$_MODULE['<{bankbca}prestashop>payment_execution_46b9e3665f187c739c55983f757ccda0'] = 'Confermo il mio ordine';
+$_MODULE['<{bankbca}prestashop>payment_execution_569fd05bdafa1712c4f6be5b153b8418'] = 'Altri metodi di pagamento';
+$_MODULE['<{bankbca}prestashop>payment_return_88526efe38fd18179a127024aba8c1d7'] = 'Il tuo ordine su %s è completo.';
+$_MODULE['<{bankbca}prestashop>payment_return_a35abd2d31d9e1da82599bc67e166506'] = 'un importo di';
+$_MODULE['<{bankbca}prestashop>payment_return_c51326a2ac458845d579a82a92f111f0'] = 'al proprietario del conto';
+$_MODULE['<{bankbca}prestashop>payment_return_de6d813f05e3916a86b89bd8b1a8a489'] = 'con questi dati';
+$_MODULE['<{bankbca}prestashop>payment_return_619b26a60df1af3649d2e8920a7949cd'] = 'a questa banca';
+$_MODULE['<{bankbca}prestashop>payment_return_13f4ac6dc75b5829499cb9cf8b733071'] = 'Ti è stata inviata una e-mail con queste informazioni.';
+$_MODULE['<{bankbca}prestashop>payment_return_c4457a9f2f20e39386008bbb2cd3a78f'] = 'Il tuo ordine verrà inviato non appena avremo ricevuto la tua transazione.';
+$_MODULE['<{bankbca}prestashop>payment_return_0db71da7150c27142eef9d22b843b4a9'] = 'Per eventuali domande o per ulteriori informazioni, contatta la nostra';
+$_MODULE['<{bankbca}prestashop>payment_return_64430ad2835be8ad60c59e7d44e4b0b1'] = 'assistenza clienti';
+$_MODULE['<{bankbca}prestashop>payment_return_8de637e24570c1edb0357826a2ad5aea'] = 'Abbiamo notato un problema con il tuo ordine. Se pensi che sia un errore, puoi contattare il nostro';
